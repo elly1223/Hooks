e813d0a13447fd8f40f0a9eade4a983b2c63b2a0
yunkyung Lee <62500947+leeykyk@users.noreply.github.com>
2021-09-06 03:07:52 +0900

    useConfirm 까지 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const useConfirm = (message = '', callback, rejection) => {
+  if (typeof callback !== 'function') {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+
+const App = () => {
+  const deleteWorld = () => console.log('Deleting the world...');
+  const abort = () => console.log('Aborted');
+  const confirmDelete = useConfirm('Are you sure?', deleteWorld, abort);
+  return (
+    <div className='App'>
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
